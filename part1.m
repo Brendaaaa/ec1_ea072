19e663ea26fc9903c5c4f334cf863189e38b6c68
brendaaaa <brenda.o.ramires@gmail.com>
2017-09-24 12:36:12 -0300

    Validation added; percentage added

--- part1.m
+++ part1.m
@@ -2,33 +2,76 @@
 % Calcula w e a soma para cada lambda...
 %
 
-function [lambda] = part1(X_tr, d_tr)
+function [w] = part1(X_tr, d_tr, X_va, d_va, initial_lambda, final_lambda, step)
 % X_tr : 40000 x 785
 % d_tr : 40000 x 10
 
+
+X_tr_T = X_tr;
+X_tr = transpose(X_tr);
+d_tr = transpose(d_tr);
+X_va = transpose(X_va);
+d_va = transpose(d_va);
+
 n = size(X_tr,2); % numero de colunas de X_tr eh a dimensao da matriz identidade
-X_tr_T = transpose(X_tr);
 correct_count = 0;
 total = size(X_tr,1);
+best_w = repelem([1],n);
+best_percentage = 0;
+best_lambda = initial_lambda;
 
-for lambda=1:2:5
+for lambda=initial_lambda:step:final_lambda
 
     w = inv(X_tr_T*X_tr  - lambda.*eye(n))*X_tr_T*d_tr;
 
     sum_total = 0;
+    correct_count = 0;
 
     for i=1:size(X_tr,1) % para cada linha da matriz, ou seja, para cada imagem
         X = X_tr(i,:); % now we have X: 1 x 784
         v2 = X*w;
         v1 = d_tr(i,:); % now we have v1 1 x 10
-        [sum] = qmean2(v1, v2);
-        %[correct, sum] = qmean2(v1, v2);
+        [correct, sum] = qmean2(v1, v2);
         sum_total = sum_total + sum;
-        %correct_count = correct_count + correct;
+        correct_count = correct_count + correct;
     end
-    % print lambda e a soma obtida
+    
     disp(lambda);
     disp(sum_total);
-    % disp(100*correct_count/total); % aqui eh a porcentagem do q esta certo
+    percentage = 100*correct_count/total;
+    disp(percentage); % aqui eh a porcentagem do q esta certo
+    if (percentage > best_percentage)
+        best_w = w;
+        best_lambda = lambda;
+        best_percentage = percentage;
+    end
     disp('    ');
 end
+
+disp('BEST IN TRAINING');
+disp(best_lambda);
+disp(sum_total);
+disp(best_percentage);
+
+
+% validation %%%%%%
+sum_total = 0;
+correct_count = 0;
+total = size(X_va,1);
+n = size(X_va,2);
+
+for i=1:size(X_va,1) % para cada linha da matriz, ou seja, para cada imagem
+    X = X_va(i,:); % now we have X: 1 x 784
+    v2 = X*best_w;
+    v1 = d_va(i,:); % now we have v1 1 x 10
+    %[sum] = qmean2(v1, v2);
+    [correct, sum] = qmean2(v1, v2);
+    sum_total = sum_total + sum;
+    correct_count = correct_count + correct;
+end
+    
+disp('VALIDATION RESULTS');
+disp(sum_total);
+percentage = 100*correct_count/total;
+disp(percentage); % aqui eh a porcentagem do q esta certo
+    
--- qmean2.m
+++ qmean2.m
@@ -2,8 +2,8 @@
 % Calcula as diferencas entre o esperado e o q obtivemos...
 %
 
-% function [correct, sum] = qmean2(v1,v2) % ao descomentar essa, comenta a debaixo
-function [sum] = qmean2(v1,v2)
+ function [correct, sum] = qmean2(v1,v2) % ao descomentar essa, comenta a debaixo
+%function [sum] = qmean2(v1,v2)
 
 sum = 0;
 correct = 0;
@@ -11,10 +11,10 @@ for i=1:size(v1) % para cada elemento do vetor
     sum = sum + (v1(i) - v2(i))^2;
 end
 
-%[M1, I1] = max(v1);
-%[M2, I2] = max(v2);
-%if (I1 == I2)
-%    correct = 1;
-%end
+[M1, I1] = max(v1);
+[M2, I2] = max(v2);
+if (I1 == I2)
+    correct = 1;
+end
 
 sum = sum/size(v1,2);
